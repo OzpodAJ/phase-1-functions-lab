50d2c36bcf0dc9e8a5715c6d23f9fcb9b993fa50
Andrew Osborn <osbornandrewj@gmail.com>
2021-09-03 13:19:55 -0700

    commit tree 1766bf62ec2fcf6649c15eb44af0c9990fabb475

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(x){
+    let y = Math.abs(x-42);
+    return y;
+}
+function distanceFromHqInFeet(x){
+    const z = distanceFromHqInBlocks(x)*264;
+    return z
+}
+function distanceTravelledInFeet(a, b){
+    let c = Math.abs((b - a)*264);
+    return c;
+}
+function  calculatesFarePrice(a, b){
+    if (distanceTravelledInFeet(a, b) <= 400)
+        return 0
+    else if (distanceTravelledInFeet(a, b) > 400 && distanceTravelledInFeet(a, b) <= 2000)
+        return 0.02*(distanceTravelledInFeet(a, b) - 400)
+    else if (distanceTravelledInFeet(a, b) > 2000 && distanceTravelledInFeet(a, b) <= 2500)
+        return 25
+    else if (distanceTravelledInFeet(a, b) > 2500)
+        return "cannot travel that far";
+}
